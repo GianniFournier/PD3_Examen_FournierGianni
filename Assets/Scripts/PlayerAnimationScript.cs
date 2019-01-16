c94508b76d475d4a0469dd83278c30964311c097
GianniFournier <43540865+GianniFournier@users.noreply.github.com>
2019-01-16 15:03:52 +0100

    Walking + Running Animation

---
+++ Assets/Scripts/PlayerAnimationScript.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationScript : MonoBehaviour {
+
+    [SerializeField]
+    private CharacterControllerBehaviour _charCTRLScript;
+
+    [SerializeField]
+    private CharacterController _charCTRL;
+
+    [SerializeField]
+    private Animator _anim;
+
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Movement();
+        CheckSprint();
+    }
+
+    private void Movement()
+    {
+        //adjust velocity to use in animation
+        Vector3 XZvel = Vector3.Scale(_charCTRL.velocity, new Vector3(1, 0, 1));
+        Vector3 localVelXZ = _charCTRLScript.gameObject.transform.InverseTransformDirection(XZvel);
+        localVelXZ.Normalize();
+
+        //set animation velocity
+        _anim.SetFloat("HorizontalVelocity", localVelXZ.x);
+        _anim.SetFloat("VerticalVelocity", localVelXZ.z);
+
+    }
+
+    private void CheckSprint()
+    {
+        if (_charCTRLScript._isSprinting == true)
+        {
+            _anim.SetBool("Sprinting", true);
+            Debug.Log("[ANIMATOR] Is Sprinting");
+        }
+
+        if (_charCTRLScript._isSprinting == false)
+        {
+            _anim.SetBool("Sprinting", false);
+            Debug.Log("[ANIMATOR] Stopped Sprinting");
+        }
+    }
+}
---
+++ Assets/Scripts/TreeScript.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeScript : MonoBehaviour {
+
+    [SerializeField]
+    bool _isTreeDestroyed = false;
+
+    [SerializeField]
+    GameObject _tree;
+
+    [SerializeField]
+    GameObject _choppedTree;
+
+
+	void Start ()
+    {
+        _tree.active = true;
+        _choppedTree.active = false;
+
+	}
+	
+	void Update ()
+    {
+		
+        if(_isTreeDestroyed == true)
+            DestroyTree();
+
+
+	}
+
+    private void DestroyTree()
+    {
+        _tree.active = false;
+        _choppedTree.active = true;
+    }
+}
