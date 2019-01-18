dc00ccdc692facadea99f9265e0de6e35cbd6a74
GianniFournier <43540865+GianniFournier@users.noreply.github.com>
2019-01-18 01:00:11 +0100

    Hakselaar

--- Assets/Scripts/LiftBehaviour.cs
+++ Assets/Scripts/LiftBehaviour.cs
@@ -1,16 +1,75 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LiftBehaviour : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private CharacterControllerBehaviour _charCTRLScript;
+
+    private bool _isButtonPressedAgain;
+
+    private Vector3 _liftUp;
+
+    void Start()
+    {
+
+        _isButtonPressedAgain = false;
+
+        _liftUp = new Vector3(50, 0, 0);
+
+    }
+
+    void Update ()
+    {
+
+        if (_charCTRLScript._isButtonPressed == true && _isButtonPressedAgain == false)
+        {
+            LiftOn();
+        }
+
+        if (_charCTRLScript._isButtonPressed == true && _isButtonPressedAgain == true)
+        {
+            LiftOff();
+        }
+
+
+    }
+
+    private void LiftOff()
+    {
+        Debug.Log("[LIFT] Lift Off");
+
+        for (int i = 0; i < 2; i++)
+        {
+            gameObject.transform.Rotate(new Vector3(-i, 0, 0));
+
+            if (gameObject.transform.rotation == Quaternion.Euler(Vector3.zero))
+            {
+                _isButtonPressedAgain = false;
+                _charCTRLScript._isButtonPressed = false;
+            }
+        }
+    }
+
+    private void LiftOn()
+    {
+        Debug.Log("[LIFT] Lift On");
+
+        for (int i = 0; i < 2; i++)
+        {
+            gameObject.transform.Rotate(new Vector3(i,0,0));
+
+            if(gameObject.transform.rotation == Quaternion.Euler(_liftUp))
+            {
+                _isButtonPressedAgain = true;
+                _charCTRLScript._isButtonPressed = false;
+            }
+        }
+
+
+            
+
+    }
 }
